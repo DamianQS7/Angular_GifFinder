82c9424dc49c78a10b3ad56576293dc71924bf5d
DamianQuezada <damianquezada.s@gmail.com>
2024-04-16 10:42:46 -0300

    Final Commit

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Gif, SearchResponse } from '../interfaces/gifs.interface';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ import { Gif, SearchResponse } from '../interfaces/gifs.interface';
 export class GifsService {
 
   public gifList: Gif[] = [];
-  private apiKey: string = '3IGUPgJHabH28emmmMUcePYCsK21unc3';
+  private apiKey: string = environment.apiKey;
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
   private _tagsHistory: string[] = [];
 
